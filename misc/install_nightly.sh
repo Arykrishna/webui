de675d2e16f31e16b861b16cd9b20a4c8d22406d
Arykrishna <63738393+Arykrishna@users.noreply.github.com>
2023-09-22 12:31:04 +0700

    Update install_nightly.sh

--- misc/install_nightly.sh
+++ misc/install_nightly.sh
@@ -37,6 +37,11 @@ git clone https://github.com/KohakuBlueleaf/a1111-sd-webui-lycoris ./extensions/
 
 git clone https://github.com/zanllp/sd-webui-infinite-image-browsing ./extensions/sd-infinite-browsing
 git clone https://github.com/Gourieff/sd-webui-reactor ./extensions/sd-webui-reactor
+#REACTOR PROBLEM SOLVE
+python -m pip install -U pip
+pip uninstall -y onnx onnxruntime onnxruntime-gpu onnxruntime-silicon onnxruntime-extensions
+pip install onnx==1.14.1 onnxruntime==1.15.1
+
 git clone https://github.com/Physton/sd-webui-prompt-all-in-one ./extensions/prompt-allinone
 git clone https://github.com/ArtVentureX/sd-webui-agent-scheduler ./extensions/agent-scheduler
 git clone https://github.com/shirayu/sd-webui-enable-checker ./extensions/enable-checker
