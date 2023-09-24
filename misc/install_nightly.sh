0f62edd070fdf695c887ee3a82cf8ea4c75eaf44
Arykrishna <63738393+Arykrishna@users.noreply.github.com>
2023-09-24 10:40:37 +0700

    Update install_nightly.sh

--- misc/install_nightly.sh
+++ misc/install_nightly.sh
@@ -40,6 +40,7 @@ git clone https://github.com/zanllp/sd-webui-infinite-image-browsing ./extension
 #git clone https://github.com/s0md3v/roop ./extensions/sd-webui-roop
 git clone https://github.com/Gourieff/sd-webui-reactor ./extensions/sd-webui-reactor
 
+git clone https://github.com/thomasasfk/sd-webui-aspect-ratio-helper ./extensions/aspect-ratio-helper
 git clone https://github.com/Physton/sd-webui-prompt-all-in-one ./extensions/prompt-allinone
 git clone https://github.com/ArtVentureX/sd-webui-agent-scheduler ./extensions/agent-scheduler
 git clone https://github.com/shirayu/sd-webui-enable-checker ./extensions/enable-checker
